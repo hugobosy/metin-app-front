f6be7616a2fbf4a8ce6bc769968985dc9807450b
Hubert <h.kliszcz@nebucode.com>
2023-09-13 17:05:00 +0200

    Add text component with styles

--- src/components/base/text/Text.tsx
+++ src/components/base/text/Text.tsx
@@ -1,14 +1,36 @@
 import { FC, HTMLProps } from "react";
 
-import styles from './Text.module.scss';
+import styles from "./Text.module.scss";
+import classNames from "classnames";
+
 export interface TextProps extends HTMLProps<HTMLParagraphElement> {
   tag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
-  text: string;
-
+  text?: string;
+  className?: string;
+  weight?: "300" | "400" | "500" | "600" | "700";
+  fontSize?: "xxs" | "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
 }
 
-export const Text: FC<TextProps> = ({ tag, text, ...rest }) => {
+export const Text: FC<TextProps> = ({
+  tag,
+  text,
+  className,
+  weight,
+  fontSize,
+  ...rest
+}) => {
   const T = tag;
 
-  return <T>{text}</T>;
+  return (
+    <T
+      {...rest}
+      className={classNames(
+        className,
+        styles[`weight-${weight}`],
+        styles[`size-${fontSize}`],
+      )}
+    >
+      {text}
+    </T>
+  );
 };
--- src/components/modules/Login/Form/Form.tsx
+++ src/components/modules/Login/Form/Form.tsx
@@ -1,7 +1,9 @@
-import styles from './Form.module.scss';
-import {Text} from "@/components/base/text/Text";
+import styles from "./Form.module.scss";
+import { Text } from "@/components/base/text/Text";
 export const Form = () => {
-    return <>
-        <Text tag="h1" text="Text" />
+  return (
+    <>
+      <Text tag="h1" text="Text" weight="300" fontSize="xxl" />
     </>
-}
+  );
+};
