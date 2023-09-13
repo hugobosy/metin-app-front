aff5e83f795a1bed8e53be5c0c45161bef934a51
Hubert <h.kliszcz@nebucode.com>
2023-09-13 16:19:55 +0200

    change login page

--- src/components/modules/Login/Form/Form.tsx
+++ src/components/modules/Login/Form/Form.tsx
@@ -1,5 +1,8 @@
 import styles from './Form.module.scss';
 import {Input} from "@/components/form/input/Input";
 export const Form = () => {
-    return <Input type="text" required label="LAbel" errorMessage="Error" />
+    return <div className={styles.wrapper}>
+        <h1>Login</h1>
+        <Input label="Label" errorMessage="Error" />
+    </div>
 }
--- src/components/modules/Login/Hero/Hero.tsx
+++
@@ -1,4 +0,0 @@
-import styles from './Hero.module.scss';
-export const Hero = () => {
-    return <h1>Hero</h1>
-}
