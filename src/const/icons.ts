99b8a10eb303db3e12701b32c2fe4d1ec9329001
Hubert Kliszcz <hubert.kliszcz@gmail.com>
2023-10-13 15:59:58 +0200

    Add flags to project

--- src/const/icons.ts
+++ src/const/icons.ts
@@ -1,1 +1,8 @@
-export const icons = {} as const;
+import Poland from "flag-icons/flags/4x3/pl.svg";
+import Germany from "flag-icons/flags/4x3/de.svg";
+import England from "flag-icons/flags/4x3/us.svg";
+export const icons = {
+  Poland,
+  Germany,
+  England,
+} as const;
