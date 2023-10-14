fe6088e0bb892e98120ee62e6bbbc52b20ea8ea7
Hubert Kliszcz <hubert.kliszcz@gmail.com>
2023-10-14 13:49:36 +0200

    add theme icons

--- src/const/icons.ts
+++ src/const/icons.ts
@@ -3,10 +3,12 @@ import England from "@/assets/svg/us.svg";
 import Germany from "@/assets/svg/de.svg";
 import Sun from "@/assets/svg/sun.svg";
 import Bell from "@/assets/svg/bell.svg";
+import Moon from "@/assets/svg/moon.svg";
 export const icons = {
   Poland,
   Germany,
   England,
   Sun,
   Bell,
+  Moon,
 } as const;
