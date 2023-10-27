c70aafa3c477b6c532d08bb84f7837fa499bb573
Hubert Kliszcz <hubert.kliszcz@gmail.com>
2023-10-27 15:30:23 +0200

    add button when balance is 0

--- src/components/layout/menu-user/menu-balance/MenuBalance.tsx
+++ src/components/layout/menu-user/menu-balance/MenuBalance.tsx
@@ -3,6 +3,7 @@ import styles from "./MenuBalance.module.scss";
 import { Text } from "@/components/base/text/Text";
 import { FC } from "react";
 import { MenuUserProps } from "../MenuUser";
+import { Button } from "@/components/base/button/Button";
 
 export interface MenuBalanceProps
   extends Pick<MenuUserProps, "balanceWon" | "balanceYang"> {}
@@ -12,37 +13,49 @@ export const MenuBalance: FC<MenuBalanceProps> = ({
   balanceYang,
 }) => {
   const t = useTranslations("Layout.balance");
+  if (balanceWon && balanceYang) {
+    return (
+      <div className={styles.wrapper}>
+        <Text
+          tag="span"
+          fontFamily="montserrat"
+          fontSize="md"
+          color="white"
+          text={t("your-balance")}
+        />
+        <Text
+          tag="span"
+          fontFamily="montserrat"
+          fontSize="md"
+          color="white"
+          text={
+            balanceWon === undefined
+              ? "Won: "
+              : "Won: " + balanceWon.toLocaleString()
+          }
+        />
+        <Text
+          tag="span"
+          fontFamily="montserrat"
+          fontSize="md"
+          color="white"
+          text={
+            balanceYang === undefined
+              ? "Yang:"
+              : "Yang: " + balanceYang.toLocaleString()
+          }
+        />
+      </div>
+    );
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <Text
-        tag="span"
-        fontFamily="montserrat"
-        fontSize="md"
-        color="white"
-        text={t("your-balance")}
-      />
-      <Text
-        tag="span"
-        fontFamily="montserrat"
-        fontSize="md"
-        color="white"
-        text={
-          balanceWon === undefined
-            ? "Won: "
-            : "Won: " + balanceWon.toLocaleString()
-        }
-      />
-      <Text
-        tag="span"
-        fontFamily="montserrat"
-        fontSize="md"
-        color="white"
-        text={
-          balanceYang === undefined
-            ? "Yang:"
-            : "Yang: " + balanceYang.toLocaleString()
-        }
-      />
-    </div>
+    <Button
+      variant="base"
+      text={t("add-balance")}
+      fontFamily="montserrat"
+      weight="700"
+      className={styles.button}
+    />
   );
 };
