41b8a2438fc9d0ad7ce15f3df2df0ba7ab2f6064
Hubert Kliszcz <hubert.kliszcz@gmail.com>
2023-11-04 13:47:46 +0100

    create Pets component

---
+++ src/components/modules/HomePage/Pets/Pets.tsx
@@ -0,0 +1,11 @@
+import { Tile } from "@/components/base/tile/Tile";
+
+import styles from "./Pets.module.scss";
+
+export const Pets = () => {
+  return (
+    <Tile>
+      <h2>Pets</h2>
+    </Tile>
+  );
+};
