e894e1d08619ac6d7bbc7de3105a33bc0f81bb35
Hubert Kliszcz <hubert.kliszcz@gmail.com>
2024-08-09 17:24:43 +0200

    add Pets and pets icon

--- src/components/modules/HomePage/Pets/Pets.tsx
+++ src/components/modules/HomePage/Pets/Pets.tsx
@@ -5,6 +5,7 @@ import { Text } from "@/components/base/text/Text";
 import { useTranslations } from "next-intl";
 import { FC } from "react";
 import { HomePageTemplateProps } from "@/components/templates/HomePageTemplate/HomePageTemplate";
+import { Pet } from "@/components/base/pet/pet";
 
 export interface PetsProps extends Pick<HomePageTemplateProps, "userPets"> {}
 
@@ -22,6 +23,9 @@ export const Pets: FC<PetsProps> = ({ userPets }) => {
         weight="500"
         className={styles["header-text"]}
       />
+      <div className={styles.pets}>
+        <Pet name="Monkey" alt="Monkey" src="" />
+      </div>
     </Tile>
   );
 };
---
+++ src/const/pets.ts
@@ -0,0 +1,17 @@
+import Monkey from "@/assets/img/pets/monkey.svg";
+
+export const pets = {
+  Monkey,
+  // Exedyar,
+  // Baashidos,
+  // Razador,
+  // Spider,
+  // Nessie,
+  // Nemere,
+  // Meley,
+  // Azrael,
+  // MiniBoss,
+  // Dragon,
+  // Kat,
+  // Alastor,
+} as const;
